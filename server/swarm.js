ff788209a12a8af0afef41099cc9d88d3741a8d7
Rafael Fávaro <rafael.faesin@gmail.com>
2014-07-21 17:00:22 -0300

    correcting swarm.js class

--- server/swarm.js
+++ server/swarm.js
@@ -1,40 +1,37 @@
-var Swarm = function() {
+Swarm = function (mpd) {
 
-	var swarmId,
-		mpdName,
-		addedPeer = false,
-		swarmList = [],
-		peerList = [],
-
-	initializeSwarm = function (mpd) {
-		// this.mpdName = mpd;
-		
-		var swarm = new Object();
-		swarm.mpdName = mpd;
-
-		console.log(JSON.stringify(swarmList));
-
-		//swarmList.
-
-	},
-
-	addPeerList = function(userId, address) {
-		this.peerList[userId].userId = userId;
-		this.peerList[userId].ipAddr = address.address;
-		this.peerList[userId].port = address.port;
-
-		this.addedPeer = true;
-	},
-
-	removePeerList = function(userId) {
-		if(this.addedPeer)
-		{
-			delete this.peerList[userId];
-		}
-	}
-
+	this.mpdName = mpd;
+	this.addedPeer = false;
+	this.peerList = [];
 };
 
+Swarm.prototype.initializeSwarm = function (mpd) {
+	// this.mpdName = mpd;
+	
+	var swarm = new Object();
+	swarm.mpdName = mpd;
+
+	console.log(JSON.stringify(swarmList));
+
+	//swarmList.
+};
+
+Swarm.prototype.addPeerList = function(userId, address) {
+	this.peerList[userId].userId = userId;
+	this.peerList[userId].ipAddr = address.address;
+	this.peerList[userId].port = address.port;
+	this.addedPeer = true;
+};
+
+Swarm.prototype.removePeerList = function(userId) {
+	if(this.addedPeer)
+	{
+		delete this.peerList[userId];
+	}
+};
+
+
+module.exports.Swarm = Swarm;
 module.exports.initializeSwarm = Swarm.initializeSwarm;
 module.exports.addPeerList = Swarm.addPeerList;
 module.exports.removePeerList = Swarm.removePeerList;

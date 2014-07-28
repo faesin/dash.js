d5cd9b79eaee795b6f479a7fc3b379f0f9925239
Rafael Fávaro <rafael.faesin@gmail.com>
2014-07-28 04:30:02 -0300

    Fixing swarm.js and server.js to actually work

--- server/server.js
+++ server/server.js
@@ -1,76 +1,73 @@
 var static = require('node-static'),
-    http = require('http'),
-    file = new(static.Server)(),
-    app = http.createServer(function (req, res) {
-        file.serve(req, res);
-    }).listen(8080),
-    io = require('socket.io').listen(app),
-    fs = require('fs');
-    swarm = require('./swarm.js')
+	http = require('http'),
+	file = new(static.Server)(),
+	app = http.createServer(function (req, res) {
+		file.serve(req, res);
+	}).listen(8080),
+	io = require('socket.io').listen(app),
+	fs = require('fs'),
+	swarm = require('./swarm.js');
 
 function handler (req, res) {
-    fs.readFile(__dirname + '/index.html', function (err, data) {
-        if (err) {
-            res.writeHead(500);
-            return res.end('Error loading index.html');
-        }
+	fs.readFile(__dirname + '/index.html', function (err, data) {
+		if (err) {
+			res.writeHead(500);
+			return res.end('Error loading index.html');
+		}
 
-        res.writeHead(200);
-        res.end(data);
-    });
+		res.writeHead(200);
+		res.end(data);
+	});
 }
 
-var Swarms = [];
-var Peers = {},
-    numSwarms = 0,
-    numPeers = 0;
+var numSwarms = 0,
+	numPeers = 0;
 
 io.sockets.on('connection', function (socket) {
-    var addedPeer = false;
-    var address = socket.handshake.address;
-    
-    console.log("Client connected from " + address.address + ":" + address.port);
+	var addedPeer = false;
+	var address = socket.handshake.address;
+	
+	console.log("Client connected from " + address.address + ":" + address.port);
 
-    socket.on('addPeer', function (data) {
-        console.log("addPeer emited: Adding peer id " + socket.id);
-        Peers[data.userId] = socket.id;
-        //Peers[data.userId].address = address.address;
-        //Peers[data.userId].port = address.port;
-        ++numPeers;
-        addedPeer = true;
+	socket.on('addPeer', function (data) {
+		console.log("addPeer emited: Adding peer id " + socket.id);
+		//Peers[data.userId] = socket.id;
+		//Peers[data.userId].address = address.address;
+		//Peers[data.userId].port = address.port;
+		swarm.addPeer(data.mpd, socket.id, address);
+		++numPeers;
+		addedPeer = true;
+	});
 
-        swarm.initializeSwarm(data.rando);
-    });
+	socket.on('getSwarmList', function (data) {
 
-    socket.on('getSwarmList', function (data) {
+	});
 
-    });
+	socket.on('getPeerList', function (data) {
+		console.log("Sending peer list to " + data.userId);
+		io.sockets.socket(Peers[data.userId].socket).emit("peer-list", Peers);
+	});
 
-    socket.on('getPeerList', function (data) {
-        console.log("Sending peer list to " + data.userId);
-        io.sockets.socket(Peers[data.userId].socket).emit("peer-list", Peers);
-    });
+	socket.on('getPeerInfo', function (data) {
+		console.log("Getting data from " + data.userId);
+	});
 
-    socket.on('getPeerInfo', function (data) {
-        console.log("Getting data from " + data.userId);
-    });
+	socket.on('getInList', function (userId) {
+		Users.push({ 'userID': userId.myId });
+		console.log("Total Users: " +  count);
+		console.log("Users Lenght: " +  Users.length);
 
-    socket.on('getInList', function (userId) {
-        Users.push({ 'userID': userId.myId });
-        console.log("Total Users: " +  count);
-        console.log("Users Lenght: " +  Users.length);
+		for(var i = 0; i < Users.length; i++)
+		{
+			console.log(JSON.stringify(Users[i]));
+		}
+	});
 
-        for(var i = 0; i < Users.length; i++)
-        {
-            console.log(JSON.stringify(Users[i]));
-        }
-    });
-
-    socket.on('disconnect', function () {
-        if(addedPeer)
-        {
-            delete Peers[socket.userId];
-            --numPeers;
-        }
-    });
+	socket.on('disconnect', function () {
+		// if(addedPeer)
+		// {
+		// 	delete Peers[socket.userId];
+		// 	--numPeers;
+		// }
+	});
 });
--- server/swarm.js
+++ server/swarm.js
@@ -1,37 +1,73 @@
-Swarm = function (mpd) {
+var addedPeer = false,
+	SwarmList = [];
 
-	this.mpdName = mpd;
-	this.addedPeer = false;
-	this.peerList = [];
-};
-
-Swarm.prototype.initializeSwarm = function (mpd) {
-	// this.mpdName = mpd;
-	
+// Swarms
+module.exports.initializeSwarm = function initializeSwarm (mpd) {
 	var swarm = new Object();
 	swarm.mpdName = mpd;
 
 	console.log(JSON.stringify(swarmList));
-
-	//swarmList.
 };
 
-Swarm.prototype.addPeerList = function(userId, address) {
-	this.peerList[userId].userId = userId;
-	this.peerList[userId].ipAddr = address.address;
-	this.peerList[userId].port = address.port;
-	this.addedPeer = true;
-};
+function getSwarmByMpd(mpd) {
+	for (var i = SwarmList.length - 1; i >= 0; i--)
+		if(SwarmList[i].mpd == mpd)
+			return SwarmList[i];
 
-Swarm.prototype.removePeerList = function(userId) {
-	if(this.addedPeer)
+	return null;
+}
+
+module.exports.addPeer = function addPeer (mpd, userId, address) {
+	console.log("addPeer emited: Adding peer to " + mpd  
+				+ " id: " + userId + " address: " + JSON.stringify(address));
+
+	var swarm = getSwarmByMpd(mpd);
+	if(swarm != null)
 	{
-		delete this.peerList[userId];
+		console.log("ADD TO EXISTING SWARM");
+	}else{
+		swarm = new Object();
+		swarm.mpd = mpd;
+		swarm.peerList = [];
+
+		var nPeer = new Object();
+		nPeer.id = userId;
+		nPeer.address = address;
+
+		swarm.peerList.push(nPeer);
+		SwarmList.push(swarm);
+
+		console.log(JSON.stringify(swarm));
 	}
+
+	addedPeer = true;
 };
 
+module.exports.yolo = function yolo () {
+	console.log("####YOLOSWAG####");
+}
 
-module.exports.Swarm = Swarm;
-module.exports.initializeSwarm = Swarm.initializeSwarm;
-module.exports.addPeerList = Swarm.addPeerList;
-module.exports.removePeerList = Swarm.removePeerList;
+// Swarm.prototype.removePeerList = function(userId) {
+// 	if(this.addedPeer)
+// 	{
+// 		delete this.peerList[userId];
+// 	}
+// };
+
+// SwarmList = function () {
+// 	this.list = [];
+// }
+
+// SwarmList.prototype.addSwarmList = function (mpd) {
+// 	var swarm = new Swarm(mpd);
+// }
+
+// SwarmList.prototype.removeSwarmList = function () {
+	
+// }
+
+// SwarmList.prototype.getSwarm = function () {
+
+
+
+// }
